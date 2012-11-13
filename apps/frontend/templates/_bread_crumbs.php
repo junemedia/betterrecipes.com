9ad69ed079f24a54d83fe9a1fec3f8193c64f4ac
Toros Tarpinyan <ttarpinyan@resoluted.com>
2012-11-13 16:27:38 -0500

    adding wallpapers to thanksgiving and christmas main categories

--- apps/frontend/templates/_bread_crumbs.php
+++ apps/frontend/templates/_bread_crumbs.php
@@ -4,19 +4,21 @@
       <? if ($url): ?>
         <a href="<?= $url ?>" title="<?= $title ?>"><?= $title ?></a>&nbsp;/
       <? else: ?>
-        <? if (in_array($title, array('thanksgiving recipes', 'christmas recipes'))) {$display_adtag = $title;} ?>
+        <? $main_category = in_array($title, array('thanksgiving recipes', 'christmas recipes')) ? $title : null ?>
         <a title="Current page" class="last"><?= $title ?></a>
-    <? endif; ?>
-  <? endforeach; ?>
+      <? endif; ?>
+    <? endforeach; ?>
   </p>
-  <? if (@$display_adtag == 'christmas recipes'): ?>
+  <? if ($main_category == 'christmas recipes'): ?>
     <span style="position:relative; right:120px; bottom:20px;">
       <script src="http://ad.doubleclick.net/N878/adj/betterrecipes.mdp.com/S1;net=md;u=,md-11469861782_1352404077,11ff8da11386969,md,;channel=S1;parent=cat740170;site=betterrecipes;child1=cat6840001;id=christmas;gender=0;age=0000;income=00;genderage=0_0000;ageincome=0000_00;genderincome=0_00;user=0_0000_00;type=slideshow;!category=pop;!c=rme;tile=3;sz=120x60;cmw=nowl;contx=md;cmd=christmas.betterrecipes.com;btg=;ord=[RANDOM]" language="javascript"></script>
     </span>
+    <? include_partial('global/wallpaper', compact('main_category')) ?>
   <? endif; ?>
-  <? if (@$display_adtag == 'thanksgiving recipes'): ?>
+  <? if ($main_category == 'thanksgiving recipes'): ?>
     <span style="position:relative; right:120px; bottom:20px;">
       <script src="http://ad.doubleclick.net/N878/adj/betterrecipes.mdp.com/S1;net=md;u=,md-11370776331_1352399433,11ff8da11386969,md,;channel=S1;parent=cat740170;site=betterrecipes;child1=cat6840001;id=thanksgiving;gender=0;age=0000;income=00;genderage=0_0000;ageincome=0000_00;genderincome=0_00;user=0_0000_00;type=slideshow;!category=pop;!c=rme;tile=3;sz=120x60;cmw=nowl;contx=md;cmd=thanksgiving.betterrecipes.com;btg=;ord=[RANDOM]" language="javascript"></script>
     </span>
+    <? include_partial('global/wallpaper', compact('main_category')) ?>
   <? endif; ?>
 <? endif; ?>
