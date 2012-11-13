7fd9395c3830d1175d8fd127ba25bbc29ac75343
Toros Tarpinyan <ttarpinyan@resoluted.com>
2012-11-13 16:27:57 -0500

    adding wallpapers to thanksgiving and christmas main categories

---
+++ apps/frontend/templates/_wallpaper.php
@@ -0,0 +1,50 @@
+<? if (@$main_category == 'christmas recipes'): ?>
+  <!-- BEGIN WALLPAPER on BR Thanksgiving -->
+  <style>
+    body {background:url('http://www.bhg.com/bhg/images/advertising/wallpaper-RCP-daisy.jpg') no-repeat scroll center 143px #BDCDDA;}
+    a#hotSpotLeft {display:block;height:538px;width:127px;position:absolute;top:213px;}
+    a#hotSpotRight {display:block;height:538px;width:127px;position:absolute;top:213px;}
+  </style>
+  <SCRIPT type="text/javascript">
+    $(document).ready(function () {
+      $('#main-content').prepend(
+      '<a id="hotSpotLeft" href="http://ad.doubleclick.net/click;h=v2|3FF7|0|0|%2a|x;264458548;0-0;0;89613609;31-1|1;51061087|51030929|1;;;pc=[TPAS_ID]%3fhttp://www.daisybrand.com/recipes/seasonal/thanksgiving.aspx" target="_blank"></a>'+
+        '<a id="hotSpotRight" href="http://ad.doubleclick.net/click;h=v2|3FF7|0|0|%2a|x;264458548;0-0;0;89613609;31-1|1;51061087|51030929|1;;;pc=[TPAS_ID]%3fhttp://www.daisybrand.com/recipes/seasonal/thanksgiving.aspx" target="_blank"></a>');
+      $('#hotSpotLeft').css('left',(($(window).width()-1000)/2)-127 );
+      $('#hotSpotRight').css('right',(($(window).width()-1000)/2)-127 );
+    });
+    $(window).resize(function() {
+      $('#hotSpotLeft').css('left',(($(window).width()-1000)/2)-127 );
+      $('#hotSpotRight').css('right',(($(window).width()-1000)/2)-127 );
+    });
+  </SCRIPT>
+  <!-- END WALLPAPER -->
+  <!-- Begin Impression Tracker -->
+  <IMG SRC="http://ad.doubleclick.net/imp;v1;f;264458548;0-0;0;89613609;1|1;51061087|51030929|1;;cs=m;pc=[TPAS_ID];%3fhttp://ad.doubleclick.net/dot.gif?%%CACHEBUSTER%%" BORDER=0 WIDTH=1 HEIGHT=1 />
+  <!-- End Impression Tracker -->
+<? endif; ?>
+<? if (@$main_category == 'thanksgiving recipes'): ?>
+  <!-- BEGIN WALLPAPER on BR Christmas -->
+  <style>
+    body {background:url('http://www.bhg.com/bhg/images/advertising/wallpaper-RCP-daisy.jpg') no-repeat scroll center 143px #BDCDDA;}
+    a#hotSpotLeft {display:block;height:538px;width:127px;position:absolute;top:213px;}
+    a#hotSpotRight {display:block;height:538px;width:127px;position:absolute;top:213px;}
+  </style>
+  <SCRIPT type="text/javascript">
+    $(document).ready(function () {
+      $('#main-content').prepend(
+      '<a id="hotSpotLeft" href="http://ad.doubleclick.net/click;h=v2|3FF7|0|0|%2a|s;264627663;0-0;0;89613624;31-1|1;51061087|51030929|1;;;pc=[TPAS_ID]%3fhttp://www.daisybrand.com/recipes/seasonal/holiday.aspx" target="_blank"></a>'+
+        '<a id="hotSpotRight" href="http://ad.doubleclick.net/click;h=v2|3FF7|0|0|%2a|s;264627663;0-0;0;89613624;31-1|1;51061087|51030929|1;;;pc=[TPAS_ID]%3fhttp://www.daisybrand.com/recipes/seasonal/holiday.aspx" target="_blank"></a>');
+      $('#hotSpotLeft').css('left',(($(window).width()-1000)/2)-127 );
+      $('#hotSpotRight').css('right',(($(window).width()-1000)/2)-127 );
+    });
+    $(window).resize(function() {
+      $('#hotSpotLeft').css('left',(($(window).width()-1000)/2)-127 );
+      $('#hotSpotRight').css('right',(($(window).width()-1000)/2)-127 );
+    });
+  </SCRIPT>
+  <!-- END WALLPAPER -->
+  <!-- Begin Impression Tracker -->
+  <IMG SRC="http://ad.doubleclick.net/imp;v1;f;264627663;0-0;0;89613624;1|1;51061087|51030929|1;;cs=y;pc=[TPAS_ID];%3fhttp://ad.doubleclick.net/dot.gif?%%CACHEBUSTER%%" BORDER=0 WIDTH=1 HEIGHT=1 />
+  <!-- End Impression Tracker -->
+<? endif; ?>
