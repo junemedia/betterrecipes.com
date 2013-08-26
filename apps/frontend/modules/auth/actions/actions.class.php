b8cd58240077e73e99e31110dd3d01216ac49b4f
Rusty Cage <rusty@citizencage.com>
2013-08-26 14:15:47 -0400

    working on login

--- apps/frontend/modules/auth/actions/actions.class.php
+++ apps/frontend/modules/auth/actions/actions.class.php
@@ -232,7 +232,7 @@ class authActions extends sfActions
     } else {
 	    //$values = $this->signinForm->getValues();
 	    //print_r($this->signinForm->getValues());
-	    print_r($this->signinForm->getFormFieldSchema());
+	    var_dump($this->signinForm->getFormFieldSchema());
     }
 
     $this->signupForm = new SignupForm();
