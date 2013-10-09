5b98383c001427864a6f159c8c03cb8373fa05a4
Benjamin Sanders <bsanders@resolute.com>
2013-10-09 09:22:46 -0400

    added google container tag

--- apps/admin/templates/layout_auth.php
+++ apps/admin/templates/layout_auth.php
@@ -16,6 +16,15 @@
     <? endif ?>
   </head>
   <body>
+	<!-- Google Tag Manager -->
+	<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-5VTT4K"
+	height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-5VTT4K');</script>
+	<!-- End Google Tag Manager -->
   	<div id="wrapper">
       <div id="masthead">
         <a href="<?= url_for('@homepage')?>"><img src="/img/meredithlogo.png" alt="meredith logo" /></a>
