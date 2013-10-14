731d84a4d156a6c6925f7056498b331e784259b7
Rusty Cage <rusty@citizencage.com>
2013-10-14 10:56:35 -0400

    update category level slideshows

--- apps/frontend/modules/recipes/templates/categorySuccess.php
+++ apps/frontend/modules/recipes/templates/categorySuccess.php
@@ -34,13 +34,12 @@ unitValues: {
   </div><!-- /#trending-recipes -->
   */
   ?>
-  <? include_component('recipes', 'catlevel_recipes_slideshows', array('category_id' => $category->getId(), 'category_name' => $category->getName(), 'is_main_cat' => true)) ?>
+  <? //include_component('recipes', 'catlevel_recipes_slideshows', array('category_id' => $category->getId(), 'category_name' => $category->getName(), 'is_main_cat' => true)) ?>
   <? //include_partial('stories', compact('stories', 'category')) ?>
   <? //include_partial('popular_stories', compact('category')) ?>
   <? include_partial('baynote_recipes', compact('category')) ?>
   <? include_partial('popular_blogs', compact('category')) ?>
-  <? /* remove slideshows as per JIRA # MERBETTERR-34 (don't ask) */ ?>
-  <? //include_component('recipes', 'catlevel_slideshows', array('category_id' => $category->getId(), 'category_name' => $category->getName())) ?>
+  <? include_component('recipes', 'catlevel_slideshows', array('category_id' => $category->getId(), 'category_name' => $category->getName())) ?>
 </div><!-- /.section -->
 <? include_partial('global/right_rail/right_rail') ?>
 <? include_partial('opengraph/facebook_login_modal') ?>
