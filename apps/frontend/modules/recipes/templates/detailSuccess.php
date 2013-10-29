229055d3ca1996894fcb9b7f8e4d82b21d8daed4
Rusty Cage <rusty@citizencage.com>
2013-10-29 16:20:14 -0400

    change infolinks wisid

--- apps/frontend/modules/recipes/templates/detailSuccess.php
+++ apps/frontend/modules/recipes/templates/detailSuccess.php
@@ -21,7 +21,7 @@ unitValues: {
 <? slot('infolinks') ?>
 <script type="text/javascript">
 var infolinks_pid = 1790157;
-var infolinks_wsid = 0;
+var infolinks_wsid = 14;
 </script>
 <script type="text/javascript" src="http://resources.infolinks.com/js/infolinks_main.js"></script>
 <? end_slot() ?>
