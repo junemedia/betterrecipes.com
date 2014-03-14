d7b8eb1c071c6c2ee70813a9de3805fc339e192b
Rusty Cage <rusty@citizencage.com>
2014-03-14 15:55:16 -0400

    testing out ad refresh

--- apps/frontend/modules/slideshows/templates/_right_rail.php
+++ apps/frontend/modules/slideshows/templates/_right_rail.php
@@ -6,7 +6,7 @@
   <div class="ad300x250">
     <? include_partial('global/adtags/300x250') ?>
   </div><!-- /.ad300x250 -->
-  <? include_partial('global/right_rail/vsw') ?>
+  <div id="vswContainer"><? include_partial('global/right_rail/vsw') ?></div><!-- // #vswContainer -->
   <? include_component('recipes', 'rr_recipes', compact('rr_recipes')) ?>
   <? include_partial('global/right_rail/contest') ?>
   
