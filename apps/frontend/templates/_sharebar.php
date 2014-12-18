8ee05e3dd78254eac3cab6a83aed83387faf728f
techyogi <bsanders@resolute.com>
2014-12-18 18:42:02 +0000

    removed print link

--- apps/frontend/templates/_sharebar.php
+++ apps/frontend/templates/_sharebar.php
@@ -1,8 +1,10 @@
 <div id="recipe-sharebar" class="mt10 border-bottom">
   <div id="inlinesharebar"<? if (isset($slideshow)): ?> class="flle"<? endif; ?>></div>
   <? if (isset($slideshow)): ?>
-    <a href="<?= url_for('@slideshow_print?regSource=8292&slug=' . $slideshow->getSlug()) ?>" id="slidesshowprintbtn" title="Print this recipe" class="btn-grey28 flle" target="_blank">Print Recipe</a>
-    <div class="view-thumbnails">
+    <!--
+	<a href="<?= url_for('@slideshow_print?regSource=8292&slug=' . $slideshow->getSlug()) ?>" id="slidesshowprintbtn" title="Print this recipe" class="btn-grey28 flle" target="_blank">Print Recipe</a>
+    -->
+	<div class="view-thumbnails">
       <? if ($sf_user->isAuthenticated()): ?>
         <a id="view_thumbnails"><? if ($showall): ?>view all<? else: ?>hide<? endif; ?> thumbnails</a>
       <? else: ?>
