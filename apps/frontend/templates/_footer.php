f6af6bbf1887038d8848f4d35a60ff1b365ea32f
techyogi <bsanders@resolute.com>
2014-12-18 21:10:18 +0000

    added comment for infolinks

--- apps/frontend/templates/_footer.php
+++ apps/frontend/templates/_footer.php
@@ -166,8 +166,13 @@ $is_hp = ($sf_request->getParameter('module') == 'home' && $host_parts[0] == 'ww
   </div>
 </div>
 </div><!-- /#main-footer -->
+
+
+
+<!-- infolinks -->
 <script type="text/javascript">
 var infolinks_pid = 1863387;
 var infolinks_wsid = 3;
 </script>
 <script type="text/javascript" src="http://resources.infolinks.com/js/infolinks_main.js"></script>
+<!-- end infolinks -->
