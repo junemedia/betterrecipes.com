310ee79d3695a4228b7a5d09447846adb9a9c076
techyogi <bsanders@resolute.com>
2014-12-19 17:46:10 +0000

    popular categories now in two columns

--- apps/frontend/modules/home/templates/_editorials.php
+++ apps/frontend/modules/home/templates/_editorials.php
@@ -1,10 +1,10 @@
 <? if ($categoryWonder && (sizeof($categoryWonder) > 0)): ?>
   <p class="title">Popular Categories</p>
-  <div id="editorials">
+  <div id="editorials" style="width:650px;">
     <? $cat1 = Doctrine_Core::getTable('Category')->find($categoryWonder->getSlotOneCatId()); ?>
     <? $subcat1 = Doctrine_Core::getTable('Category')->find($categoryWonder->getSlotOneSubcatOne()); ?>
     <? $subcat2 = Doctrine_Core::getTable('Category')->find($categoryWonder->getSlotOneSubcatTwo()); ?>
-    <div class="mr40">
+    <div >
       <div class="header">
         <img src="<?= $cat1->getImgSrc() ?>" alt="<?= $cat1->getName() ?>"  />
         <p class="mb10"><a href="<?= getUrl($cat1) ?>" title="<?= $cat1->getName() ?>"><?= $cat1->getName() ?></a></p>
@@ -32,7 +32,7 @@
     <? $cat3 = Doctrine_Core::getTable('Category')->find($categoryWonder->getSlotThreeCatId()); ?>
     <? $subcat1 = Doctrine_Core::getTable('Category')->find($categoryWonder->getSlotThreeSubcatOne()); ?>
     <? $subcat2 = Doctrine_Core::getTable('Category')->find($categoryWonder->getSlotThreeSubcatTwo()); ?>
-    <div class="mr40">
+    <div >
       <div class="header">
         <img src="<?= $cat3->getImgSrc() ?>" alt="<?= $cat3->getName() ?>"  />
         <p class="mb10"><a href="<?= getUrl($cat3) ?>" title="<?= $cat3->getName() ?>"><?= $cat3->getName() ?></a></p>
