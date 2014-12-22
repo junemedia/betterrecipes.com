a806718bc5c698a2b2dff885d4179483e6078d26
techyogi <bsanders@resolute.com>
2014-12-22 21:10:27 +0000

    added ATF OpenX ad units

--- apps/frontend/templates/_right_rail/right_rail_loggedin.php
+++ apps/frontend/templates/_right_rail/right_rail_loggedin.php
@@ -1,7 +1,29 @@
 <div class="sidebar">
   <div id="rr_user_area"></div>
   <div class="ad300x250">
-    <? include_partial('global/adtags/300x250') ?>
+    <!--/* OpenX Asynchronous JavaScript tag */-->
+
+	<!-- /*
+	 * The tag in this template has been generated for use on a
+	 * non-SSL page. If this tag is to be placed on an SSL page, change the
+	 * 'http://ox-d.junemedia.com/...'
+	 * to
+	 * 'https://ox-d.junemedia.com/...'
+	 */ -->
+
+	<div id="537278268_300x250_ATF" style="width:300px;height:250px;margin:0;padding:0">
+	  <noscript><iframe id="ea73408dac" name="ea73408dac" src="http://ox-d.junemedia.com/w/1.0/afr?auid=537278268&cb=INSERT_RANDOM_NUMBER_HERE" frameborder="0" scrolling="no" width="300" height="250"><a href="http://ox-d.junemedia.com/w/1.0/rc?cs=ea73408dac&cb=INSERT_RANDOM_NUMBER_HERE" ><img src="http://ox-d.junemedia.com/w/1.0/ai?auid=537278268&cs=ea73408dac&cb=INSERT_RANDOM_NUMBER_HERE" border="0" alt=""></a></iframe></noscript>
+	</div>
+	<script type="text/javascript">
+	  var OX_ads = OX_ads || [];
+	  OX_ads.push({
+		 slot_id: "537278268_300x250_ATF",
+		 auid: "537278268"
+	  });
+	</script>
+
+	<script type="text/javascript" src="http://ox-d.junemedia.com/w/1.0/jstag"></script>
+	<!-- end openx -->
   </div><!-- /.ad300x250 -->
   <? include_component('recipes', 'rr_recipes', compact('rr_recipes')) ?>
   <? include_partial('global/right_rail/contest') ?>
