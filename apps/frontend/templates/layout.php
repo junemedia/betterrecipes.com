96012344c3aa7a08aa7408eaea3ed45725e86029
techyogi <bsanders@resolute.com>
2015-01-30 15:39:10 +0000

    added gtm container tag

--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -122,15 +122,15 @@
 
   </head>
   <body>
-  <!-- Google Tag Manager -->
-  <noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-5VTT4K"
-  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-  <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-  '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-  })(window,document,'script','dataLayer','GTM-5VTT4K');</script>
-  <!-- End Google Tag Manager -->
+    <!-- Google Tag Manager -->
+	<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-W6SMSQ"
+	height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-W6SMSQ');</script>
+	<!-- End Google Tag Manager -->
     <? /*<script src="http://cdn.yb0t.com/p/d45f/js/interstitial-config.js"></script>*/ ?>
     <div id="fb-root"></div>
     <script>(function(d, s, id) {
@@ -170,18 +170,7 @@
     
     <!--<script>Meebo('domReady');</script>-->
 
-    <? /* Google Analytics */ ?>
-    <script>
-    var _gaq = _gaq || [];
-    _gaq.push(['_setAccount', 'UA-29730487-1']);
-    _gaq.push(['_setDomainName', 'betterrecipes.com']);
-    _gaq.push(['_trackPageview']);
-    (function() {
-      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-    })();
-    </script>
+   
     <script>
     /* <![CDATA[ */
     var _mb_site_guid = document.location.href.indexOf('resolute.com') > 0 ? '537a2a0e4370d27f28b3d4f6704f8ccb7f3f6a37cb35e81b058cb9a83642cc18' : '2dcab8563a168a3da605b518134a9fef5ede02393d2484f05f9727d7f5ee36f7';
--- web/blogs/daily-dish/wp-content/themes/BetterRecipes2/header.php
+++ web/blogs/daily-dish/wp-content/themes/BetterRecipes2/header.php
@@ -95,6 +95,15 @@ gigya.socialize.addEventHandlers({ onConnectionAdded: gigya.reporting.onSendDone
 </script>
   </head>
   <body>
+<!-- Google Tag Manager D -->
+<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-W6SMSQ"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-W6SMSQ');</script>
+<!-- End Google Tag Manager -->
     <!-- start body -->
     <!-- Begin Meebo sharebar open span -->
     <span id="meeboAfterBody"></span>
