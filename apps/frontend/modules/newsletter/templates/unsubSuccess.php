f00d1381fd53b36f494e3023af9193e13ab1d0f1
techyogi <bsanders@resolute.com>
2015-03-23 07:28:52 +0000

    Howe's Changes:update br unsub process

--- apps/frontend/modules/newsletter/templates/unsubSuccess.php
+++ apps/frontend/modules/newsletter/templates/unsubSuccess.php
@@ -23,7 +23,7 @@
                 <table width="600" cellpadding="0" cellspacing="0" border="0" class="quickNewsUserInfo">
 				<tr><td style="height:10px;"></td></tr>
         <tr><td style="color:#000;">
-    <iframe width="575" height="700" frameborder="0" src="http://r4l.popularliving.com/subctr/unsub/br/entry.php?listid=<?php echo $lid;?>&amp;jobid=<?php echo $jid?>&amp;email=<?php echo $email;?>" id="new_unsubscribe" scrolling="No"></iframe>
+    <iframe width="575" height="700" frameborder="0" src="http://r4l.popularliving.com/subctr/unsub/br/unsub.php?listid=<?php echo $lid;?>&amp;jobid=<?php echo $jid?>" id="new_unsubscribe" scrolling="No"></iframe>
     </td></tr>
 
                 </table>
