d8d0e520231f6d4049fd335c9936974ab5f70f9b
techyogi <bsanders@resolute.com>
2015-03-27 19:16:39 +0000

    removed ntynt ads

--- apps/frontend/templates/_footer.php
+++ apps/frontend/templates/_footer.php
@@ -77,10 +77,12 @@ $is_hp = ($sf_request->getParameter('module') == 'home' && $host_parts[0] == 'ww
 
 <!-- BEGIN SiteCTRL Script tynt tag -->
 <script type="text/javascript">
+/*
 if(document.location.protocol=='http:'){
  var Tynt=Tynt||[];Tynt.push('cvyMIgHsSr5lQVacwqm_6l');
  (function(){var s=document.createElement('script');s.async="async";s.type="text/javascript";s.src='http://tcr.tynt.com/ti.js';var h=document.getElementsByTagName('script')[0];h.parentNode.insertBefore(s,h);})();
 }
+*/
 </script>
 <!-- END SiteCTRL Script -->
 
