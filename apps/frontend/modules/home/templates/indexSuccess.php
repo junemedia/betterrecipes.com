2d40acd3e71738c32ec58b3c491f2b095daf3b50
John Shearer <john@ultranaut.com>
2017-05-31 20:50:27 +0000

    Restore Media.net unit on homepage for time-being

--- apps/frontend/modules/home/templates/indexSuccess.php
+++ apps/frontend/modules/home/templates/indexSuccess.php
@@ -31,11 +31,18 @@
 
   <? include_partial('editorials', compact('categoryWonder')) ?>
 
+  <!-- begin media.net 6 -->
+  <script id="mNCC" language="javascript">  medianet_width='650';  medianet_height= '175';  medianet_crid='662071020';  </script>  <script id="mNSC" src="http://contextual.media.net/nmedianet.js?cid=8CU52X6SM" language="javascript"></script>
+  <div class="clear-both"></div>
+  <!-- end media.net -->
+
+  <?php if (false) { // take out for now ?>
   <!-- DEFY VIDEO -->
   <div class="clear-both" style="margin: 30px auto;">
     <?php include_partial("global/adtags/defy-clip_1848") ?>
   </div>
   <div class="clear-both"></div>
+  <?php } ?>
 
   <?php include_partial('global/adtags/outbrain_TF_4') ?>
 
