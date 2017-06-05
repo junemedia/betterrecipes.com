ee8865b1e7895118d5b9d6e1918774d7d3115c8b
John Shearer <john@ultranaut.com>
2017-06-05 21:17:15 +0000

    Break Comscore tag out into partial

--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -245,21 +245,8 @@
     </script>
     <script src="http://c5.zedo.com/jsc/c5/frd.js"></script>
 
-    <!-- Begin comScore Tag -->
-    <script>
-      var _comscore = _comscore || [];
-      _comscore.push({ c1: "2", c2: "6036305" });
-      (function() {
-        var s = document.createElement("script"), el = document.getElementsByTagName("script")[0]; s.async = true;
-        s.src = (document.location.protocol == "https:" ? "https://sb" : "http://b") + ".scorecardresearch.com/beacon.js";
-        el.parentNode.insertBefore(s, el);
-      })();
-    </script>
-    <noscript>
-      <img src="http://b.scorecardresearch.com/p?c1=2&amp;c2=6036305&amp;cv=2.0&amp;cj=1" />
-    </noscript>
-    <!-- End comScore Tag -->
 
+    <? include_partial('global/comscore') ?>
     <? include_partial('global/swoop') ?>
     <? include_partial('global/underdog') ?>
     <? include_partial('global/liveconnect') ?>
