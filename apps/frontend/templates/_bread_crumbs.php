18972f27ac5c67fc33722ace6c4fd2dec783fe5e
John Shearer <john@ultranaut.com>
2017-06-08 15:44:52 +0000

    Remove old 120x60 Doubleclick ads

--- apps/frontend/templates/_bread_crumbs.php
+++ apps/frontend/templates/_bread_crumbs.php
@@ -9,16 +9,4 @@
       <? endif; ?>
     <? endforeach; ?>
   </p>
-  <? if ($main_category == 'christmas recipes'): ?>
-    <span style="position:relative; right:120px; bottom:12px;">
-      <script src="http://ad.doubleclick.net/N3865/adj/betterrecipes.mdp.com/S1;net=md;u=,md-11469861782_1352404077,11ff8da11386969,md,;channel=S1;parent=cat740170;site=betterrecipes;child1=cat6840001;id=christmas;gender=0;age=0000;income=00;genderage=0_0000;ageincome=0000_00;genderincome=0_00;user=0_0000_00;type=slideshow;!category=pop;dcopt=ist;tile=3;sz=120x60;cmw=nowl;contx=md;cmd=christmas.betterrecipes.com;btg=;ord=[RANDOM]" language="javascript"></script>
-    </span>
-    <? include_partial('global/wallpaper', compact('main_category')) ?>
-  <? endif; ?>
-  <? if ($main_category == 'thanksgiving recipes'): ?>
-    <span style="position:relative; right:120px; bottom:12px;">
-      <script src="http://ad.doubleclick.net/N3865/adj/betterrecipes.mdp.com/S1;net=md;u=,md-11370776331_1352399433,11ff8da11386969,md,;channel=S1;parent=cat740170;site=betterrecipes;child1=cat6840001;id=thanksgiving;gender=0;age=0000;income=00;genderage=0_0000;ageincome=0000_00;genderincome=0_00;user=0_0000_00;type=slideshow;!category=pop;dcopt=ist;tile=3;sz=120x60;cmw=nowl;contx=md;cmd=thanksgiving.betterrecipes.com;btg=;ord=[RANDOM]" language="javascript"></script>
-    </span>
-    <? include_partial('global/wallpaper', compact('main_category')) ?>
-  <? endif; ?>
 <? endif; ?>
