ec6a27303a20fcc6a8d18ef54b322281cdaf8599
John Shearer <john@ultranaut.com>
2017-06-08 18:55:34 +0000

    Break Zergnet 2x2 unit out into partial

---
+++ apps/frontend/templates/_adtags/zergnet_2x2.php
@@ -0,0 +1,17 @@
+<style> #zergnet-widget-29457 div{ margin-top:0px; } </style>
+
+<div style="font-family:Arial color:#000000; font-size:138.5%; margin: 20px 0 -10px 5px; text-transform: uppercase;">Around The Web</div>
+
+<?php /* actual Zergnet code... */ ?>
+<div id="zergnet-widget-29457" style="width:300px;"></div>
+
+<script language="javascript" type="text/javascript">
+  (function() {
+    var zergnet = document.createElement('script');
+    zergnet.type = 'text/javascript'; zergnet.async = true;
+    zergnet.src = 'http://www.zergnet.com/zerg.js?id=29457';
+    var znscr = document.getElementsByTagName('script')[0];
+    znscr.parentNode.insertBefore(zergnet, znscr);
+  })();
+</script>
+
--- apps/frontend/templates/_right_rail/vsw.php
+++ apps/frontend/templates/_right_rail/vsw.php
@@ -1,43 +1,16 @@
+<div class="vswAdContainer roundedCornerWithWhiteBG" id="vsw">
 
-  <div class="vswAdContainer roundedCornerWithWhiteBG" id="vsw">
-  
-                <style type="text/css">
-                    body div.vsw-ad-rc .vsw-ad-title {color:#2C73BB !important; font-family:Arial !important;}
-                    body div.vsw-ad-rc .vsw-ad-text {font-family:Arial !important; font-size:12px !important; color:black !important;}
-                    body div.vsw-ad-rc .vsw-ad-domain {font-family:Arial !important; color:black !important; font-size:11px !important;}
-                </style>
-                <!--
-				<span class="heading2">More Smart Savings</span>
-                -->
-				<div id="ERA_AD_BLOCK1"></div>
-				
-				
-				<style>
-					#zergnet-widget-29457 div{
-						margin-top:0px;
-					}
-				</style>
-				
-                 <!-- ZERGNET ADS 2 -->		 
-				 
-				<div style="font-family:Arial color:#000000; font-size:18px; margin-top:20px; margin-bottom:-10px; margin-left:5px; text-transform: uppercase;">Around The Web</div>
+  <style type="text/css">
+    body div.vsw-ad-rc .vsw-ad-title {color:#2C73BB !important; font-family:Arial !important;}
+    body div.vsw-ad-rc .vsw-ad-text {font-family:Arial !important; font-size:12px !important; color:black !important;}
+    body div.vsw-ad-rc .vsw-ad-domain {font-family:Arial !important; color:black !important; font-size:11px !important;}
+  </style>
 
-				<div id="zergnet-widget-29457" style="width:300px;"></div>
+  <div id="ERA_AD_BLOCK1"></div>
 
-				<script language="javascript" type="text/javascript">
-				(function() {
-				var zergnet = document.createElement('script');
-				zergnet.type = 'text/javascript'; zergnet.async = true;
-				zergnet.src = 'http://www.zergnet.com/zerg.js?id=29457';
-				var znscr = document.getElementsByTagName('script')[0];
-				znscr.parentNode.insertBefore(zergnet, znscr);
-				})();
-				</script>
+  <?php include_partial('global/adtags/zergnet_2x2'); ?>
 
-				
-				<!-- End ZERGNET ADS -->
-				
   <?php if ($sf_params && $sf_params->get('module') == 'recipes'):
     include_partial('global/adtags/openx_300x600');
   endif; ?>
-</div>	
+</div>
