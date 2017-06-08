b14ace20ef854c2106e3ca892eece11ff1cd9ea0
John Shearer <john@ultranaut.com>
2017-06-08 19:25:27 +0000

    Remove an old "wallpaper" ad

--- apps/frontend/templates/_bread_crumbs.php
+++ apps/frontend/templates/_bread_crumbs.php
@@ -4,7 +4,6 @@
       <? if ($url): ?>
         <a href="<?= $url ?>" title="<?= $title ?>"><?= $title ?></a>&nbsp;/
       <? else: ?>
-        <? $main_category = in_array($title, array('thanksgiving recipes', 'christmas recipes')) ? $title : null ?>
         <a title="Current page" class="last"><?= $title ?></a>
       <? endif; ?>
     <? endforeach; ?>
