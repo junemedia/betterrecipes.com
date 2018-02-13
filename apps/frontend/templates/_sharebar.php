330995d1e4bb26da6489c693781ea9baedb19c0d
John Shearer <john@ultranaut.com>
2018-02-13 21:06:56 +0000

    Slideshow whitespace cleanup

--- apps/frontend/templates/_sharebar.php
+++ apps/frontend/templates/_sharebar.php
@@ -1,22 +1,16 @@
 <div id="recipe-sharebar" class="mt10 border-bottom" style="display:none;">
-  <div id="inlinesharebar"<? if (isset($slideshow)): ?> class="flle"<? endif; ?>></div>
+  <div id="inlinesharebar" <? if (isset($slideshow)): ?>class="flle"<? endif; ?>></div>
   <? if (isset($slideshow)): ?>
-    <!--
-	<a href="<?= url_for('@slideshow_print?regSource=8292&slug=' . $slideshow->getSlug()) ?>" id="slidesshowprintbtn" title="Print this recipe" class="btn-grey28 flle" target="_blank">Print Recipe</a>
-    -->
-	<div class="view-thumbnails">
-      <? if ($sf_user->isAuthenticated()): ?>
-        <a id="view_thumbnails"><? if ($showall): ?>view all<? else: ?>hide<? endif; ?> thumbnails</a>
-      <? else: ?>
-	  <!--
-        <a href="<?= url_for('@slideshow_all?regSource=8292&slug=' . $slideshow->getSlug()) ?>" onclick="appendPageNo(this)">view all thumbnails</a>
-		-->
-      <? endif; ?>
+    <div class="view-thumbnails">
+    <? if ($sf_user->isAuthenticated()): ?>
+      <a id="view_thumbnails"><? if ($showall): ?>view all<? else: ?>hide<? endif; ?> thumbnails</a>
+    <? endif; ?>
     </div>
   <? endif; ?>
 </div>
+
 <script>
-  $(document).ready(function(){
+  $(document).ready(function () {
     brmb.gigya.sharebar();
   });
 </script>
